b4b8d94a1dc0d23c183fdb9526fffc72cb69f8cb
selftok-team <selftok-team@huawei.com>
2025-04-24 03:09:33 +0800

    use mathjax in scrollable table

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,10 +7,19 @@ import react from "@astrojs/react";
 
 import svelte from "@astrojs/svelte";
 
+import remarkMath from 'remark-math';
+import rehypeMathjax from 'rehype-mathjax';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon(), mdx(), react(), svelte()],
   markdown: {
+    remarkPlugins: [
+      remarkMath,
+    ],
+    rehypePlugins: [
+      rehypeMathjax,
+    ],
     shikiConfig: {
       themes: {
         light: 'github-light',
